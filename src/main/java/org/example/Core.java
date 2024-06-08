a4f184acaa5ed7c8552e2c419eb880fa73c66337
Zandrex <zandrex@mail.ru>
2024-06-08 12:53:06 +0300

    edit

--- src/main/java/org/example/Core.java
+++ src/main/java/org/example/Core.java
@@ -1,2 +1,42 @@
-package org.example;public class Core {
+package org.example;
+
+import lombok.SneakyThrows;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Core extends Thread{
+    private Form f;
+    private People p;
+    private static volatile HashMap<Integer, Boolean> result = new HashMap<>();
+    private boolean res;
+    private static AtomicInteger key = new AtomicInteger(0);
+
+    public Core(){
+        f = new Form();
+        p = new People(f);
+    }
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        System.out.println(STR."Started \{Thread.currentThread().getName()}");
+        process();
+    }
+
+    public void process() throws InterruptedException{
+        for (int i = 0; i < 100; i++) {
+            res = p.choicePeople();
+            putResult(res);
+        }
+    }
+    public synchronized void putResult(boolean res){
+        int k = key.incrementAndGet();
+        System.out.println(k + " " + res);
+        result.put(k, res);
+    }
+
+    public static void printResult(){
+        System.out.println(result);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,15 @@
 package org.example;
 
 public class Main {
+    static void startAll(){
+        for (int i = 0; i < 10; i++) {
+            Core c = new Core();
+            c.start();
+        }
+    }
     public static void main(String[] args) {
-        View v = new View();
-        v.startRes();
+        startAll();
+        System.out.println("_______");
+        Core.printResult();
     }
 }
